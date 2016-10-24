2ae46754a118fd29076bd274d5f4ce61f4c16f0e
Mary Olive <mary.olive@me.com>
2016-10-24 15:26:52 -0400

    Initial commit

---
+++ Lab 1/Perimeter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Perimeter
+    {
+       public float length;
+       public float width;
+
+        public float Area = (length * width);
+        public float Perimeter = ((length * 2) + (width * 2));
+    }
+
+}
---
+++ Lab 1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string uContinue = ("y");
+            //if (uContinue == "y")
+            while (uContinue == "y")
+            {
+
+               // capture user input
+                Console.WriteLine("Enter Length:    ");
+                string uLength = Console.ReadLine();
+                double length = Convert.ToDouble(uLength);
+
+                Console.WriteLine("Enter Width:   ");
+                string uWidth = Console.ReadLine();
+                double width = Convert.ToDouble(uWidth);
+
+                //process
+                double area = (length * width);
+                double perimeter = ((2 * length) + (2 * width));
+
+
+                //output
+                Console.WriteLine("Area = " + area);
+                Console.WriteLine("Perimeter = " + perimeter);
+
+                Console.WriteLine("Continue? (y/n)");
+                uContinue = Console.ReadLine();
+            }
+                       
+         }
+    }
+}
