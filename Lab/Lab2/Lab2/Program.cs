73b961ccd8baa1622d17f800288ba918b138a681
Mary Olive <mary.olive@me.com>
2016-10-26 15:22:02 -0400

    Update Lab 3

--- Lab/Lab2/Lab2/Program.cs
+++ Lab/Lab2/Lab2/Program.cs
@@ -20,27 +20,38 @@ namespace Lab2
                     case "begin": //establish setting
                         Console.WriteLine("The carnival atmosphere is thick with the smell of airborne sugar and sweat.  Your senses are overwhelmed with the frenetic vibration of crowd energy and the repeated boom of 'STEP RIGHT UP!'");
                         Console.WriteLine("");
-                        Console.WriteLine("You're due at the Ferris Wheel in 10 minutes to meet your little brother, but you're still hoping to rescue your evening with at least one worthwhile experience. You cast about, trying to parse the blooming, buzzing confusion of this lame but oddly carnivalesque middle school carnival.");
+                        Console.WriteLine("You're due at the Ferris Wheel in 10 minutes to meet your little brother, but you're still hoping to rescue your evening with at least one worthwhile experience. You cast about, trying to parse the blooming, buzzing confusion of this lame but oddly creepy middle school carnival.");
                         Console.WriteLine("");
-                        Console.WriteLine("Suddenly, you're caught offgaurd when a bulbous face appears just inches from your own.  Hot, sulpherous breath and words explode like a firecracker from the gin-blossomed face. 'AND WHAT'S YOUR NAME, CHILD?' demands the face.");
-                        Console.WriteLine("Before you even have time to take offense at being called a child, you blurt out:");
-                        string uName = Console.ReadLine();
-                        Console.WriteLine("The face disappears as quickly as it had appeared, carried away on the tide of the crowd.");
-                        Console.WriteLine("Feeling a little silly, you roll your eyes and continue walking");
-                        state = "games";
-                        break;
+                        //Console.WriteLine("Suddenly, you're caught offgaurd when a bulbous face appears just inches from your own.  Hot, sulpherous breath and words explode like a firecracker from the gin-blossomed face. 'AND WHAT'S YOUR NAME, CHILD?' demands the face.");
+                        //Console.WriteLine("Before you even have time to take offense at being called a child, you blurt out:");
+                        //string uName = Console.ReadLine();
+                        //Console.WriteLine("The face disappears as quickly as it had appeared, carried away on the tide of the crowd.");
+                        //Console.WriteLine("Feeling a little silly, you roll your eyes and continue walking");
+                        
+                        Console.Write("Would you like to continue? (y/n)  ");
+                        string proceed = Console.ReadLine();
+                        proceed = proceed.ToLower();
+                        if (proceed == "y")
+                        {
+                            state = "games";
+                        }
+                        else
+                        {
+                            state = "ferrisWheel";
+                        }
+                       break;
 
                     case "games":  //present games, get choice
-                        Console.Clear();
-                        Console.WriteLine("'Win a prize!  Guess the number of marbles in the jar!!' blasts at you from your left.");
-                        Console.WriteLine("'Take your chance!!  Dunk an already wet and sad carny!!' cries a voice to your right.");
-                        Console.WriteLine("'Answer the call of Destiny,_uName_. Ignore this call at your own risk,' hisses a low voice that seems to come from just inside the right side of your skull.");
+                                   // Console.Clear();
+                        Console.WriteLine("You continue to walk.");
                         Console.WriteLine("");
-                        Console.WriteLine("Do you want to play the Marbles game?");
-                        Console.WriteLine("Do you want to play the Dunk game?");
-                        Console.WriteLine("Do you want to answer the call of Destiny?");
-                        Console.WriteLine("Do you just want to Leave?");
-                        Console.WriteLine("Please respond with 'Marbles', 'Dunk', 'Destiny' or 'Leave':  ");
+                        Console.WriteLine("");
+                        Console.WriteLine("To your left, a voice booms, 'Win a prize!  Guess the number of marbles in the jar!!'");
+                        Console.WriteLine("'Take your chance!!  Dunk an already wet and sad carny!!' cries a voice to your right.");
+                        Console.WriteLine("'Your Destiny beckons. Ignore this call at your own risk,' hisses a low voice that seems to come from just inside the right side of your skull.");
+                        Console.WriteLine("");
+                        Console.WriteLine("Would you like to play the Marbles game (M), the Dunk game (D), answer the Call of destiny (C) or just Leave (L)?");
+                        
                         string uGame = Console.ReadLine();
                         if (uGame == "Marbles")
                         {
@@ -49,6 +60,10 @@ namespace Lab2
                         else if (uGame == "Dunk")
                         {
                             state = "dunk";
+                        }
+                        else if (uGame == "Destiny")
+                        {
+                            state = "destiny";
                         }
                         else if (uGame == "Leave")
                         {
@@ -82,6 +97,7 @@ namespace Lab2
                         }
                         break;
                     case "dunk": //dunk game
+                        Console.Clear();
                         Console.WriteLine("'Step right up!' yells the game conductor, handing you a dirty projectile.  'Dunk this sad fella in 3 tries and win a prize!!'");
                         Console.WriteLine("You take the ball reticently and take aim.");
                         Console.WriteLine("Your first try is a hit.  The poor, wet man falls once again into the tank.");
@@ -90,6 +106,7 @@ namespace Lab2
                         break;
                      
                     case "destiny":  //enter gypsy's tent
+                        Console.Clear();
                         Console.WriteLine("As you make your way to the Tarot Reader's tent, you glance at your watch.  Ain't nobody got time for that!");
                         state = "ferrisWheel";
                         break;
--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -13,14 +13,19 @@ namespace Lab3
             // get input 
             Console.WriteLine("Enter an integer:  ");
             int num = Convert.ToInt32(Console.ReadLine());
-          
-            
+
             //generate first column 
+            Console.WriteLine("Number".PadLeft(5).PadRight(10) + "Squared".PadRight(10) + "Cubed");
+            Console.WriteLine("=======".PadLeft(5).PadRight(10) + "=======".PadRight(10) + "========");
+            //Console.WriteLine(string.Format("{ 0; -10} {1;-10} {2;-10}"), "=======", "=======", "=======");
+
             for (int i = 1; i <= num; i++)
             {
                 double square = Math.Pow(i, 2);
                 double cube = Math.Pow(i, 3);
-                Console.WriteLine(i + "     " + square + "       " + cube);
+                //Console.WriteLine("    " + i + "          " + square + "          " + cube);
+                //Console.WriteLine(sI.PadLeft(5).PadRight(20) +  square + "       " + cube);
+                Console.WriteLine(string.Format("{ 0: -10} {1:-10} {2:-10}"), i, square, cube);
             }
 
             //print table
