72c144421a9e555476e23cbc36ac6d312e5c80b3
Mary Olive <mary.olive@me.com>
2016-10-26 07:20:04 -0400

    Fill in some content

--- Lab/Lab2/Lab2/Program.cs
+++ Lab/Lab2/Lab2/Program.cs
@@ -11,32 +11,43 @@ namespace Lab2
         static void Main(string[] args)
         {
 
-            string state = "1";
+            string state = "begin";
             bool gameOn = true;
             while (gameOn)
             {
                 switch (state)
                 {
-                    case "begin": //establish carnival setting, intro 3 games
-                       
+                    case "begin": //establish setting
+                        Console.WriteLine("The carnival atmosphere is thick with the smell of airborne sugar and sweat.  Your senses are overwhelmed with the frenetic vibration of crowd energy and the repeated boom of 'STEP RIGHT UP!'");
+                        Console.WriteLine("");
+                        Console.WriteLine("You're due at the Ferris Wheel in 10 minutes to meet your little brother, but you're still hoping to rescue your evening with at least one worthwhile experience. You cast about, trying to parse the blooming, buzzing confusion of this lame but oddly carnivalesque middle school carnival.");
+                        Console.WriteLine("");
+                        Console.WriteLine("Suddenly, you're caught offgaurd when a bulbous face appears just inches from your own.  Hot, sulpherous breath and words explode like a firecracker from the gin-blossomed face. 'AND WHAT'S YOUR NAME, CHILD?' demands the face.");
+                        Console.WriteLine("With hardly a thought, you blurt out:");
+                        string uName = Console.ReadLine();
+                        Console.WriteLine("The face disappears as quickly as it had appeared, carried away on the tide of the crowd.");
+                        Console.WriteLine("Feeling a little silly, you roll your eyes and continue walking");
+                        state = "games";
                         break;
 
-                    case "games":  //choose game; get name
-                        Console.WriteLine("The air around you is thick with sucrose particles, crowd energy and the repeated sound of 'STEP RIGHT UP!'");
-                        Console.WriteLine("'Win a prize!  Guess the number of marbles in this jar!!' comes a voice from your left.");
-                        Console.WriteLine("'Your chance to dunk an already wet and sad carny!!' cries someone to your right.");
-                        Console.WriteLine("'Learn deep truths about yourself and your destiny,' intimates a more salient voice that seems to come from just inside your skull.");
+                    case "games":  //present games
+                        Console.WriteLine("'Win a prize!  Guess the number of marbles in the jar!!' blasts at you from your left.");
+                        Console.WriteLine("'Take your chance!!  Dunk an already wet and sad carny!!' cries a voice to your right.");
+                        Console.WriteLine("'Answer the call of Destiny," + uName + ", Ignore this call at your own risk,' hisses a low voice that seems to come from just inside the right side of your skull.");
                         break;
                     case "ferrisWheel":  //exit to ferris wheel
                         gameOn = false;
                         break;
-                    case "guess":  //play guess the marbles game
-
+                    case "marbles":  //play guess the marbles game
+                        Console.WriteLine("You eye the mason jar full of marbles.")
                         break;
                     case "4a": //
                      
                     case "tarot":  //enter gypsy's tent
-                    default:
+                    */
+                    default 'gameout':
+                        Console.WriteLine("Maybe you'll just head back toward the ferris wheel and wait for your brother... meh.. I guess this evening was a wash, after all.");
+                        gameOn = false;
                         break;
                 }
 
