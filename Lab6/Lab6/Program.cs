2d35ff1f744a7924a1c08839aa1793ea911fde44
Mary Olive <mary.olive@me.com>
2016-10-29 09:40:06 -0400

    Lab6: Cleaner (overcommitting)

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -10,48 +10,48 @@ namespace Lab6
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Pig Latin Translator");
+            Console.WriteLine("(igpay atinlay anslatortray)");
+            Console.WriteLine("  ");
 
             //set up proceed loop 
             string proceed = "y";
-
             while (proceed == "y")
             {
-            //Get user input
-            Console.WriteLine("Pig Latin Translator");
-                Console.WriteLine("(igpay atinlay anslatortray)");
-                Console.WriteLine("  ");
-            //StringBuilder PL = new StringBuilder();
-            Console.WriteLine("Please enter a word:  ");
-            string word = Console.ReadLine();
-            //PL = PL.Append(Console.ReadLine());
+                //Get user input
+                Console.WriteLine("Please enter a word:  ");
+                string word = Console.ReadLine();
+                word = word.ToLower();
 
-            //to lowercase
-            word = word.ToLower();
-                //Console.WriteLine(word);
-
-                //starts w/ vowel?  process if yes (+way)
-                char[] vowel = {'a','e','i','o','u' };
-            if (vowel.Contains(word[0]))
-            {
-                string way = (word + "way");
-                Console.WriteLine(way);
-            }
-                //process if no(move substring before 1st vowel to end, add 'ay')
-            else 
-              {
-                int wIndex = word.IndexOfAny(vowel);        //find index of first vowel
-                string ig = word.Substring(wIndex);
-                string pig = ig + word.Substring(0, (wIndex));
-                Console.WriteLine(pig + "ay");
-            }
-            Console.WriteLine("Translate another word? (y/n)");
-            proceed = Console.ReadLine();
+                //if word starts with a vowel (+way)
+                char[] vowel = { 'a', 'e', 'i', 'o', 'u' };
+                if (vowel.Contains(word[0]))
+                {
+                    string way = (word + "way");
+                    Console.WriteLine(way);
+                }
+                //if word starts with a consonant (move substring before 1st vowel to end, add 'ay')
+                else
+                {
+                    int wIndex = word.IndexOfAny(vowel);        //find index of first vowel
+                    string ig = word.Substring(wIndex);
+                    string pig = ig + word.Substring(0, (wIndex));
+                    Console.WriteLine(pig + "ay");
+                }
+                Console.WriteLine("Translate another word? (y/n)");
+                proceed = Console.ReadLine();
+                proceed = proceed.ToLower();
+                if (proceed == "yes")
+                {
+                    proceed = "y";
+                }
+                
             }
             Console.WriteLine("oodbyegay!");
         }
 
         //NOTES:  words with only 'y' as a vowel (e.g., shy, my) throw exception.  fix.
-        
+        // Next, (Extend) Keep case.
     }
-    }
+}
 
